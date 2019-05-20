1411d7d2b48b20f63e0360775ad3612a0d5e199a
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-20 17:03:20 -0300

    03 consuming restful

---
+++ 03-consuming-a-restful-web-service/readme.md
@@ -0,0 +1,7 @@
+# Java + Springframework
+
+__Consuming a RESTful Web Service__
+
+- [guide] https://spring.io/guides/gs/consuming-rest/
+- [repo]  https://github.com/spring-guides/gs-consuming-rest.git
+
---
+++ 03-consuming-a-restful-web-service/src/main/java/hello/Application.java
@@ -0,0 +1,40 @@
+package hello;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String args[]) {		
+		SpringApplication.run(Application.class);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		Properties props = System.getProperties();
+		props.put("http.proxyHost", "proxylatam.indra.es");
+		props.put("http.proxyPort", "8080");
+
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
+}
